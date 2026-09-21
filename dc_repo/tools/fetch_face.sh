1e2185b8924ffd9e5cfc5898d917ecfbca026318
eBioDomeProjects <thompson.ryan.e@gmail.com>
2026-09-20 22:06:59 -0400

    Delete dc_repo directory

--- dc_repo/tools/fetch_face.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Re-download face-api.js + weights into face/ (run from the repo root)
-set -e; B=https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master; mkdir -p face/models
-curl -sSfL -o face/face-api.min.js $B/dist/face-api.min.js
-for f in tiny_face_detector_model-weights_manifest.json tiny_face_detector_model-shard1 face_landmark_68_tiny_model-weights_manifest.json face_landmark_68_tiny_model-shard1 face_recognition_model-weights_manifest.json face_recognition_model-shard1 face_recognition_model-shard2; do curl -sSfL -o face/models/$f $B/weights/$f; done
-echo "face/ ready"
